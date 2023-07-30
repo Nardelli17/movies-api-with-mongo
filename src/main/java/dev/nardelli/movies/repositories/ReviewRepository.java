d988c6a679109567ae8bfb38cc39dd2f9d6ace10
Nardelli17 <vitornardelli17>
2023-07-30 19:18:01 -0300

    First commit

---
+++ src/main/java/dev/nardelli/movies/repositories/ReviewRepository.java
@@ -0,0 +1,10 @@
+package dev.nardelli.movies.repositories;
+
+import dev.nardelli.movies.entity.Review;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+}
---
+++ src/main/java/dev/nardelli/movies/services/MovieService.java
@@ -0,0 +1,22 @@
+package dev.nardelli.movies.services;
+
+import dev.nardelli.movies.entity.Movie;
+import dev.nardelli.movies.repositories.MovieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MovieService {
+    @Autowired
+    private MovieRepository repository;
+
+    public List<Movie> findAllMovies() {
+        return repository.findAll();
+    }
+    public Optional<Movie> findMovieByImdbId(String imdbId) {
+        return repository.findMovieByImdbId(imdbId);
+    }
+}
